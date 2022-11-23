dd982f345193478b5192ee277ccbaadc5b8ad99e
Nicolas Valencia <nicolasvalenciamadrid@gmail.com>
2022-11-22 21:48:36 -0500

    home

--- web/src/main/java/co/edu/uniquindio/unicine/bean/InicioBean.java
+++ web/src/main/java/co/edu/uniquindio/unicine/bean/InicioBean.java
@@ -1,18 +1,36 @@
 package co.edu.uniquindio.unicine.bean;
 
+import co.edu.uniquindio.unicine.entidades.Pelicula;
+import co.edu.uniquindio.unicine.servicios.servicios.ClienteServicio;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import java.io.Serializable;
+import java.util.List;
 
 @Component
 @ViewScoped
 public class InicioBean implements Serializable {
 
+    @Autowired
+    private ClienteServicio clienteServicio;
+
     @Getter
     @Setter
-    private String mensaje = "mi pagina";
+    private List<Pelicula> peliculasCartelera;
+
+
+    @PostConstruct
+    public void init() {
+        try {
+            peliculasCartelera = clienteServicio.listarPeliculasCartelera(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
